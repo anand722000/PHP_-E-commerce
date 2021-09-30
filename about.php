d2a89d904e99650d8d072c4b253311e1da662a25
anand722000 <59029929+anand722000@users.noreply.github.com>
2021-09-30 14:17:24 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,24 @@
+<?php include('include/home/header.php'); ?>
+	
+	<section id="form">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4 col-sm-offset-1">
+					<div class="login-form">
+						<h2>About Us</h2>
+						<address>
+	    					<p>I am a B.Tech Student and during summer week traning under the guidence of Splen technology 
+							I worked on this project (E-commerce website using PHP) and I hope you enjoy our projects as well
+							as our product also. If you have any questions or comments, please don't hesitate to contact us..</p>
+							<p>Address: India</p>
+							<p>Mobile: 7888788...</p>
+							<p>Email: anand722000@gmail.com</p>
+	    				</address>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	
+<?php include('include/home/footer.php'); ?>
